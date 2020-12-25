49c9fd84a9e59b97646519e8c6f1593f0ff8b5fa
DanishOnGit <youcandoit1911@gmail.com>
2020-12-25 18:48:57 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+var inputprice=document.querySelector("#inputprice")
+var inputquant=document.querySelector("#inputquant")
+var outputpercent=document.querySelector("#outputpercent")
+var outputabs=document.querySelector("#outputabsolute")
+var submit=document.querySelector("#clickbtn")
+
+
+
+function check(){
+
+    var stockprice=inputprice.value;
+    var stockquant=inputquant.value;
+    
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+submit.addEventListener("click",check)
+
